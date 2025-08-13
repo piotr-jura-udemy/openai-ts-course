984a8a513bff23c0044a12308a8e2dfcdf863616
Piotr Jura <jura.piotrek@gmail.com>
2025-08-13 19:45:18 +0200

    017-rag-chunks

--- chat-with-rag.ts
+++ chat-with-rag.ts
@@ -18,15 +18,44 @@ interface DocumentChunk {
 
 let knowledgeBase: DocumentChunk[] = [];
 
-function loadKB(): Omit<DocumentChunk, "embedding"> {
+function loadKB(): Omit<DocumentChunk, "embedding">[] {
   const kbDir = join(process.cwd(), "knowledge");
   const files = readdirSync(kbDir).filter((f) => f.endsWith(".md"));
 
-  const docs: Omit<DocumentChunk, "embedding"> = [];
+  const docs: Omit<DocumentChunk, "embedding">[] = [];
 
   for (const file of files) {
     const content = readFileSync(join(kbDir, file), "utf-8");
+
+    const chunkSize = 1000;
+    const chunks: string[] = [];
+
+    for (let i = 0; i < content.length; i += chunkSize) {
+      const slice = content.slice(i, i + chunkSize).trim();
+      if (slice.length > 0) chunks.push(slice);
+    }
+
+    if (chunks.length > 1) {
+      const last = chunks[chunks.length - 1]!;
+
+      if (last?.length < 200) {
+        chunks[chunks.length - 2] = `${chunks[chunks.length - 2]} ${last}`;
+        chunks.pop();
+      }
+    }
+
+    chunks.forEach((chunk) => {
+      docs.push({
+        content: chunk.trim(),
+        metadata: {
+          filename: file,
+        },
+      });
+      console.log(`${file} chunk length: ${chunk.length}`);
+    });
   }
+
+  return docs;
 }
 
 let conversationHistory =
@@ -34,6 +63,8 @@ let conversationHistory =
 
 console.log("🤖 ChatGPT Clone 1.0");
 console.log("💡 Type 'exit' or 'quit' to end chat\n");
+
+loadKB();
 
 while (true) {
   const userMessage = await rl.question("You: ");
